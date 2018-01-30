390faec3f8e65be33ed3bc0db53da93b8215bb39
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-01-30 14:46:35 +0900

    Solve Q2

--- src/main/scala/me/shoma/backlog/user/migration/Main.scala
+++ src/main/scala/me/shoma/backlog/user/migration/Main.scala
@@ -3,8 +3,10 @@ package me.shoma.backlog.user.migration
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import cats.implicits._
-import backlog4s.apis.AllApi
-import backlog4s.datas.AddUserForm
+import backlog4s.apis.{AllApi, UserApi}
+import backlog4s.datas.{AddUserForm, User}
+import backlog4s.dsl.ApiDsl
+import backlog4s.dsl.ApiDsl.ApiPrg
 import backlog4s.dsl.syntax._
 import backlog4s.interpreters.AkkaHttpInterpret
 import me.shoma.backlog.user.migration.command.{CommandLineArgsParser, Config, OptionParser}
@@ -13,10 +15,39 @@ import scala.util.{Failure, Success}
 
 object Main extends App {
 
+  import ApiDsl.HttpOp._
+
   CommandLineArgsParser.parse(OptionParser, args).attempt.map {
     case Right(config) => start(config)
     case Left(_)       => sys.exit(1)
   }.unsafeRunSync()
+
+  def fetchUsers(api: UserApi, offset: Int, limit: Int): ApiPrg[Seq[User]] = {
+    def go(acc: Seq[User], current: Int): ApiPrg[Seq[User]] = {
+      api.all(current).orFail.flatMap { users =>
+        if (users.nonEmpty)
+          go(acc ++ users, current + 100)
+        else
+          pure(acc)
+      }
+    }
+    go(Seq(), 0)
+  }
+
+  type UserStreamF[A] = (Seq[User], Int, Int) => ApiPrg[A]
+
+  def streamUser[A](api: UserApi, offset: Int, limit: Int)(f: UserStreamF[A]): ApiPrg[A] = {
+    def go(current: Int): ApiPrg[A] = {
+      api.all(current, limit).orFail.flatMap { users =>
+        if (users.nonEmpty)
+          f(users, current, limit).flatMap(_ => go(current + limit))
+        else
+          f(users, current, limit)
+      }
+    }
+    go(0)
+  }
+
 
   def start(config: Config): Unit = {
 
@@ -28,11 +59,8 @@ object Main extends App {
     val srcApi = AllApi.accessKey(s"${config.srcBacklogUrl}/api/v2/", config.srcBacklogKey)
     val dstApi = AllApi.accessKey(s"${config.dstBacklogUrl}/api/v2/", config.dstBacklogKey)
 
-    // Q3. Good solution to output progress.
-
-    val result = for {
-      users <- srcApi.userApi.all(limit = 1000).orFail // Q2. How to get large number of users with FP?
-      createdUsers <- users.map { user =>
+    val result = streamUser(srcApi.userApi, 0, 2) { (users, current, limit) =>
+      users.map { user =>
         val form = AddUserForm(
           userId = user.userId.getOrElse(""),
           // Q4. I'm considering to encrypt the password.
@@ -44,8 +72,10 @@ object Main extends App {
           roleType = user.roleType
         )
         dstApi.userApi.create(form).orFail
+        // Q3. Good solution to output progress. IO modad
+        // println(current + "/" + limit)
       }.sequence
-    } yield createdUsers
+    }
 
     result.foldMap(httpInterpret).onComplete { result =>
       result match {
